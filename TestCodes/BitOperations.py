439a89720f157fccea88111c201b35df84380223
Amal Shrivastava <amal.shrivastava@gmail.com>
2021-05-28 18:44:41 +0530

    rearrangement

--- TestCodes/BitOperations.py
+++ TestCodes/BitOperations.py
@@ -1,3 +1,5 @@
+## If all numbers in the array repeat except for one, find the element which does not repeat
+
 def FindOddOneOut(A):
     xorsum = 0
 
@@ -8,3 +10,6 @@ def FindOddOneOut(A):
 
 
 print(FindOddOneOut([2,3,4,5,9,8,8,9,4,3,2]))
+
+
+
