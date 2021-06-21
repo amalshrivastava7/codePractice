d60903b7daab3b61c922ff54881eba0f95f89530
Amal Shrivastava <amal.shrivastava@gmail.com>
2021-06-21 23:32:02 +0530

    Test codes

--- DS Questions/Randomize.py
+++ DS Questions/Randomize.py
@@ -6,19 +6,24 @@ df = pd.read_csv("/Users/amal/Desktop/C2C.csv")
 
 ## Print columns in a dataframe
 
-print(df.columns)
+# print(df.columns)
 
 
 ## Index or specify row, columns to select
 
-print(df[1:10][["CIFNUMMER", "ACCOUNTKEY", "BRANCH_NBR"]])
+# print(df[1:10][["CIFNUMMER", "ACCOUNTKEY", "BRANCH_NBR"]])
 
 
 ## Sample display
 
-print(df.sample(frac=0.5))
+# print(df.sample(frac=0.5))
+#
+# print(df.sample(axis = 1))
 
-print(df.sample(axis = 1))
+print(df[:10]['LAST_YEAR_AVERAGE_BALANCE'])
+
+print(df[:10].LAST_YEAR_AVERAGE_BALANCE.max())
+print(df[:10].LAST_YEAR_AVERAGE_BALANCE.min())
 
 
 
--- TestCodes/Test1.py
+++ TestCodes/Test1.py
@@ -1,10 +1,28 @@
 import numpy as np
 
-a = np.ones(10)
-c = np.zeros(5)
-d = np.append(a,c)
-print(type(a))
-print(a)
-print(d)
+# a = np.ones(10)
+# c = np.zeros(5)
+# d = np.append(a,c)
+# print(type(a))
+# print(a)
+# print(d)
+#
+# print(d.tolist())
 
-print(d.tolist())
+
+
+
+# a = np.array([1,2,3,4])
+#
+# b = np.array([1,2,3,4])
+#
+# print(a @ b)
+# print(b.T)
+
+
+a = [1,2,3,4]
+b = [5,6,7]
+
+# for i,j in zip(a,
+
+print(a, "ddd", b)
